746f0e047ff11c20e472cf9fafeae67529ebde92
lina0322 <llim@student.42seoul.kr>
2021-04-27 16:15:25 +0900

    init: 프로젝트 생성(iPhone, 세로모드 only)

---
+++ 42sparta_ios/42sparta_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  42sparta_ios
+//
+//  Created by 리나 on 2021/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
