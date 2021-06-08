ec439e5a9e7d11dfe719cb9283192b1abfb67462
ChoiKanghun <rkdgns4562@naver.com>
2021-06-08 14:54:39 +0900

    install: cocoapods로 RxSwift, RxCocoa  설치

---
+++ 42sparta_ios/Pods/Target Support Files/Pods-42sparta_ios-42sparta_iosUITests/Pods-42sparta_ios-42sparta_iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_42sparta_ios_42sparta_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_42sparta_ios_42sparta_iosUITests
+@end
---
+++ 42sparta_ios/Pods/Target Support Files/Pods-42sparta_iosTests/Pods-42sparta_iosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_42sparta_iosTests : NSObject
+@end
+@implementation PodsDummy_Pods_42sparta_iosTests
+@end
